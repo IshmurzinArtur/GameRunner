2871d80463c32ed8e4cf487e29d69b20a126abb0
IshmurzinArtur <89225651308ia@gmail.com>
2018-02-10 22:19:30 +0500

    Added arrow

--- Assets/Scripts/BoxControl.cs
+++ Assets/Scripts/BoxControl.cs
@@ -10,16 +10,23 @@ public class BoxControl : MonoBehaviour {
 	}
 	public GameObject arrow;
 	public bool AllGood = false;
+	public GameObject player;
+	bool haveArrow = false;
+	GameObject myArrow;
 	// Update is called once per frame
 	void Update () {
-		if (GameObject.Find("Arrow") != null)
+		if (player.transform.position.x - this.transform.position.x <= 4 && !haveArrow)
 		{
-			arrow = GameObject.Find("Arrow");
-			ArrorControl pt = arrow.GetComponent<ArrorControl>();
+			myArrow = Instantiate(arrow, new Vector3(this.transform.position.x, this.transform.position.y + 2, 0), Quaternion.Euler(0,0,90));
+			haveArrow = true;
+		}
+		if (myArrow != null)
+		{
+			ArrorControl pt = myArrow.GetComponent<ArrorControl>();
 			if (pt.Tap == true)
 			{
 				AllGood = true;
-				Destroy(arrow);
+				Destroy(myArrow);
 			}
 		}
 	}
---
+++ Assets/Scripts/TrrigerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrrigerControl : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject box;
+	public bool youCanJump = false;
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		BoxControl boxScript = box.GetComponent<BoxControl>;
+		if (boxScript.AllGood == true)
+			youCanJump = true;
+	}
+}
